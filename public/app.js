76214b4abfd9e3f346f71f444a8558bfdc410f1c
Darryl Kilzer <darrylkilzer@gmail.com>
2022-04-14 10:51:18 -0600

    finished

--- public/app.js
+++ public/app.js
@@ -1,16 +1,88 @@
 // if you want to deploy or use ngrok, you want to use the static version of your html being served on the server, with just a '/' here in the io.connection.
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("/");
+let roomID = ''
+let game = {}
 
 //New Game Created Listener
 socket.on("newGame", (data) => {
-    console.log('new game created: ', data);
+    roomID = data.roomID
 })
 
 
-//Join Game Event Emitter
+socket.on(`joined`, data => {
+    roomID = data.roomID
+})
 
+socket.on('updateGameState', data => {
+    game = data
+    console.log(game)
+    drawGameArea()
+})
 
 //Select Choice function
-
+function makeChoice(choice) {
+    socket.emit(`makeChoice`, { choice, roomID })
+}
 
 //Result Event Listener
+
+function createGame(e) {
+    e.preventDefault()
+    socket.emit('createGame', { name: e.target.player1Name.value })
+}
+
+//Join Game Event Emitter
+function joinGame(e) {
+    e.preventDefault()
+    socket.emit('joinGame', { name: e.target.player2Name.value, roomID: e.target.roomID.value })
+    console.log(socket.id)
+}
+
+
+function playAgain() {
+    socket.emit('playAgain', { roomID })
+}
+
+function drawGameArea() {
+    document.getElementById('game-selection').hidden = true
+    let el = document.getElementById('game')
+    let template = /*HTML*/`
+    <div class="leaderboard">
+        <h1>Room ID: ${roomID}</h1>
+        <h2>Welcome, ${game.players[socket.id].name}</h2>
+        <h3>Score:</h3>
+        `
+
+    for (const key in game.players) {
+        const player = game.players[key];
+        template += /*HTML*/`
+            <h4 class="name">
+                    ${player.name}:
+                    <span>${player.score}</span>
+                </h4>`
+
+    }
+
+    template += /*HTML*/`       
+    </div>
+    <div class="controls">
+        <button onclick="makeChoice('rock')" class="btn btn-primary">
+            Rock
+       </button>
+       <button onclick="makeChoice('paper')" class="btn btn-info">
+           Paper
+       </button>
+       <button onclick="makeChoice('scissors')" class="btn btn-danger">
+           Scissors
+       </button>
+    </div>
+    <div id="message">
+        ${game.players[socket.id].message}
+    </div>
+    <br>`
+
+    template += game.over ? /*HTML*/ `<button class="btn btn-success" onclick="playAgain()">Play Again</button>` : ''
+
+    el.innerHTML = template
+}
+
--- server/main.js
+++ server/main.js
@@ -25,26 +25,94 @@ io.on("connection", (socket) => {
     console.log("connection established ", socket.id);
 
     //Create Game Listener
-    socket.on("createGame", (data) => {
+    socket.on("createGame", data => {
         const roomID = Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, 5);
         socket.join(roomID);
-        rooms[roomID] = { player1: { name: data.name, choice: '' }, player2: { name: '', choice: '' } };
+        rooms[roomID] = { over: false, players: {} };
+        rooms[roomID].players[`${socket.id}`] = { name: data.name, choice: '', message: 'Make a choice...', score: 0 }
         socket.emit("newGame", { roomID: roomID });
+        socket.emit('updateGameState', rooms[roomID])
         console.log(rooms);
     })
 
     //Join Game Listener
+    socket.on('joinGame', data => {
+        if (!rooms[data.roomID]) {
+            return
+        }
+        rooms[data.roomID].players[socket.id] = { name: data.name, choice: '', message: 'Make a choice...', score: 0 }
+        socket.emit('joined', { roomID: data.roomID })
+        socket.broadcast.emit('updateGameState', rooms[data.roomID])
+        socket.emit('updateGameState', rooms[data.roomID])
+        console.log(socket.id)
+        console.log(rooms)
+    })
 
     //Listener for Player 1's Choice
-
-
     //Listener to Player 2's Choice
+    socket.on('makeChoice', data => {
+        if (!rooms[data.roomID]) {
+            return
+        }
+        let player = rooms[data.roomID].players[socket.id]
+        player.choice = player.choice ? player.choice : data.choice
+        player.message = player.message == 'Make a choice...' ? `You chose ${player.choice}` : player.message
+        evaluateGame(socket.id, data.roomID)
+        socket.broadcast.emit('updateGameState', rooms[data.roomID])
+        socket.emit('updateGameState', rooms[data.roomID])
+        console.log(data, rooms)
+    })
 
 
-    //evaluate resultafter getting both choices
+    socket.on('playAgain', data => {
+        if (!rooms[data.roomID]) {
+            return
+        }
+        const game = rooms[data.roomID]
+        game.over = false
+        for (const key in game.players) {
+            const player = game.players[key];
+            player.choice = ''
+            player.message = 'Make a choice...'
+        }
+        socket.broadcast.emit('updateGameState', game)
+        socket.emit('updateGameState', game)
+    })
 
+    //evaluate result after getting both choices
 
 })
 
+let choices = ['rock', 'paper', 'scissors']
+let outcomes = ['You win!', 'You lose!', 'It was a tie']
 
 // you can create functions out here to help find the winner, etc
+function evaluateGame(player1SocketID, roomID) {
+    const game = rooms[roomID]
+    let player1Choice = ''
+    let player2Choice = ''
+    let player1 = {}
+    let player2 = {}
+    for (const socketID in game.players) {
+        if (socketID == player1SocketID) {
+            player1Choice = choices.indexOf(game.players[socketID].choice)
+            player1 = game.players[socketID]
+        } else {
+            player2Choice = choices.indexOf(game.players[socketID].choice)
+            player2 = game.players[socketID]
+        }
+        if (game.players[socketID].choice == '') {
+            return
+        }
+    }
+    if (!game.over) {
+        game.over = true
+        let player1OutcomeIndex = (player1Choice - player2Choice + 2) % 3
+        let player2OutcomeIndex = (player2Choice - player1Choice + 2) % 3
+        player1.score += player1OutcomeIndex == 0 ? 10 : 0
+        player2.score += player2OutcomeIndex == 0 ? 10 : 0
+
+        player1.message = `${player2.name} chose ${player2.choice}, you chose ${player1.choice}. ${outcomes[player1OutcomeIndex]}`
+        player2.message = `${player1.name} chose ${player1.choice}, you chose ${player2.choice}. ${outcomes[player2OutcomeIndex]}`
+    }
+}
